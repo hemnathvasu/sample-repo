2ac21093b3704e7aed7a9c53210c75159fa49117
hemnathvasu <hemnathvasu.v@gmail.com>
2024-09-14 12:49:14 +0530

    Updated codes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world git and gti hub");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Git-hub");
+        }
     }    
 }
