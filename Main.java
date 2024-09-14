92b6a48d9eb1c9ebae07dae11c7097677c63a03c
hemnathvasu <hemnathvasu.v@gmail.com>
2024-09-14 12:47:38 +0530

    Code changes

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Add another file-commit");
+       print();
+    }
+
+    public static void print(){
+        System.out.println("Hello world");
     }
 }
